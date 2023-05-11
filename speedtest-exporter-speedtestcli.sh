9f7210bf67f18813d85264f91b6bb12129abfc5b
Jeffrey Bekker <Jeffrey44113@users.noreply.github.com>
2023-05-11 10:34:18 +0200

    just upload and download.

--- speedtest-exporter-speedtestcli.sh
+++ speedtest-exporter-speedtestcli.sh
@@ -12,10 +12,6 @@ speedtest_output=$(speedtest-cli --csv)
 # Iterate over each line of the speedtest output and process the metrics
 echo "$speedtest_output" | while IFS=',' read -r serverid servername timestamp latency jitter packetloss download upload download_bytes upload_bytes share_url; do
     # Print out the metrics using the printMetric function
-    printMetric "speedtest_latency_seconds" "Latency" "gauge" "$latency"
-    printMetric "speedtest_jitter_seconds" "Jitter" "gauge" "$jitter"
     printMetric "speedtest_download_bytes" "Download Speed" "gauge" "$download"
     printMetric "speedtest_upload_bytes" "Upload Speed" "gauge" "$upload"
-    printMetric "speedtest_downloadedbytes_bytes" "Downloaded Bytes" "gauge" "$download_bytes"
-    printMetric "speedtest_uploadedbytes_bytes" "Uploaded Bytes" "gauge" "$upload_bytes"
 done
